d215f34431d0f521eaa1cb627dedbebb6d68a11a
Pasang Nuru Sherpa <79486506+solu22@users.noreply.github.com>
2022-03-08 05:07:55 +0200

    database setup heroku

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const taskRouter = require("./routes");
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello backend");
-});
+app.get('/', (req, res)=>{
+  res.send('hello backend')
+})
 
 /* Routes */
 
 app.use("/tasks", taskRouter);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`Server listening at port ${port}`));
